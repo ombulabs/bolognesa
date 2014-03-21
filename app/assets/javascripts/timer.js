9d3a9adcdeee6724207c8611509cec4c77c17698
Lucas Mourelle <info@lucasmourelle.com.ar>
2014-03-21 15:23:43 -0300

    Timer and sound

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,7 +1,38 @@
 // Bolognesa timer
 // ***************
 
+var interval_id;
+
 function setPercentage(percent){
+	var _percent = Number(percent);
 	$('#timer-progress').attr("value", percent);
 	$('.visible-progress-value').html(percent);
 }
+function getPercentage(){
+	var percent_value = Number( $('#timer-progress').attr("value") );
+	return percent_value;
+}
+function addPercentage(){
+	if(getPercentage() >= 100){
+		stop();
+		playDing();
+	} else {
+		var percent_to_add = 1;
+		setPercentage(getPercentage() + percent_to_add);
+	}
+}
+function start(){
+	interval_id = setInterval(addPercentage,100);
+}
+function stop(){
+	clearInterval(interval_id);
+}
+function playDing(){
+	document.getElementById('audio-ding').play();
+}
+
+
+
+$(document).ready(function(){
+	$('.start-button').click(start);
+});
