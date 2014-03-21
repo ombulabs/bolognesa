eb591d5e58905ad2b386ad61e5e942f4ab04177d
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-03-21 11:58:01 -0300

    Pomodoro model and controller.

---
+++ app/models/pomodoro.rb
@@ -0,0 +1,5 @@
+class Pomodoro < ActiveRecord::Base
+
+  belongs_to :user
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :authorizations
+  has_many :pomodori
 
   def create_provider(auth_hash)
     unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
