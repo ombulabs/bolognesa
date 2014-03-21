7dd7e4aed78ed2ec32517aebf7b3ebb66a392c8d
Lucas Mourelle <info@lucasmourelle.com.ar>
2014-03-21 13:54:17 -0300

    Más arreglos de HTML y CSS.

---
+++ app/assets/javascripts/notices.js
@@ -0,0 +1,20 @@
+// Show and hide notices
+
+function drawFlashMessage(message_text, message_type){
+  // message type: notice, warning, error
+  if($("#flash-container").length > 0){
+    $("#flash-container").html("");
+    $("#flash-container").append('<div class="flash ' + message_type + '" style="display: none;">' + message_text + '</div>');
+  }
+  showFlashMessages();
+}
+
+function showFlashMessages(){
+  if($(".notice").length > 0){
+    $(".notice").hide().delay(1000).slideDown(1000).delay(9000).slideUp(1000);
+  }
+}
+
+$(document).ready(function(){
+	showFlashMessages();
+});
