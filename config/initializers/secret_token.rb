0a9cc96c942f1f87dee2a1523af7400413c38aa9
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-03-21 09:53:28 -0300

    Empty app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolognesa::Application.config.secret_token = '8aeac6fcaed7f5ed48a57581b1bc0363325b837ee42489cc239a99f4643c9a60e5bf615005848c10aee763e4f50cccb50da2f43b23cf61b3936d6d1d9b3932d7'
