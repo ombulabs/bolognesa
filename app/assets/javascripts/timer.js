6774b9597f6421e01ad19dc21a7c0c45c0e175de
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-03-21 15:25:14 -0300

    Ajax call for start button.

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -5,3 +5,9 @@ function setPercentage(percent){
 	$('#timer-progress').attr("value", percent);
 	$('.visible-progress-value').html(percent);
 }
+
+$(document).ready(function() {
+  $('button').on('click', function () {
+    $.ajax("/pomodoris/create")
+	});
+});
