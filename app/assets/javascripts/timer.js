01edbb71f98dd2aa75f43b1d01a3391feec33a00
Lucas Mourelle <info@lucasmourelle.com.ar>
2014-03-28 13:14:12 -0300

    Timer programming

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,15 +1,23 @@
 // Bolognesa timer
 // ***************
 
-var interval_id;
+var current_interval_id;
 
-var show_remaining_time_as_percentage = true;
+var current_start_date;
+var current_finish_date;
+
+var show_remaining_time_as_percentage = false;
 
 // Sets the percentage
-function setPercentage(percent){
+function setPercentage(percent, remaining_time){
 	var _percent = Number(percent);
 	$('#timer-progress').attr("value", percent);
-	$('.visible-progress-value').html(Math.floor(percent) + "%");
+	if(show_remaining_time_as_percentage){
+		$('.visible-progress-value').html(Math.floor(percent) + "%");
+	} else {
+		var remaining_time_string = remaining_time.getMinutes() + ":" + remaining_time.getSeconds();
+		$('.visible-progress-value').html(remaining_time_string);
+	}
 }
 
 // Gets the percentage
@@ -25,9 +33,15 @@ function addPercentage(){
 	if(getPercentage() >= 100){
 		finishPomodoro();
 	} else {
-		// var percent_to_add = 100 / (25*60); // el posta
-		var percent_to_add = 100 / (25*60) * 100; // provisorio para testing
-		setPercentage(getPercentage() + percent_to_add);
+		var _percent;
+		var _remaining_time = new Date();
+
+		var percent_to_add = 100 / (25*60); // el posta
+		// var percent_to_add = 100 / (25*60) * 100; // provisorio para testing
+
+		_percent = getPercentage() + percent_to_add;
+		_remaining_time.setTime(current_finish_date.getTime() - 1000);
+		setPercentage(_percent, _remaining_time);
 	}
 }
 
@@ -41,14 +55,27 @@ function finishPomodoro(){
 // Starts counting working time
 function start(){
 	stop();
-	setPercentage(0);
-	interval_id = setInterval(addPercentage,1000);
+	setPercentage(0, 0);
+	current_interval_id = setInterval(addPercentage,1000);
   $.ajax("/pomodoris/create");
+
+  current_start_date = new Date();
+  current_finish_date = new Date();
+  current_finish_date.setTime(current_start_date.getTime()+(25*60*1000));
+}
+
+// Time remaining until the end of the pomodoro
+// Returns Date object
+function getRemainingTime(){
+	var now = new Date();
+	var difference = new Date();
+	difference.setTime(now.getTime() - current_finish_date.getTime());
+	return difference;
 }
 
 // Stops timer
 function stop(){
-	clearInterval(interval_id);
+	clearInterval(current_interval_id);
 }
 
 // Plays ding sound, using HTML5 <audio> tag
