9b19465ee61ac42b22ebe1386894a5d64bbd013f
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-03-28 15:29:40 -0300

    Modal now working.

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,22 @@
+// Pomodoro modal
+// ***************
+
+$(document).ready(function() {
+
+  var $modal, $modal_close, $modal_container;
+
+  $modal = $('#modal');
+  $modal_close = $modal.find('.close');
+  $modal_container = $('#modal-container');
+
+  $('.edit_tags').click(function(xhr, data, status) {
+    $modal.html(data).prepend($modal_close).css('top', $(window).scrollTop() + 40).show();
+    $modal_container.show();
+  });
+
+  $(document).on('click', '#modal .close', function() {
+    $modal_container.hide();
+    $modal.hide();
+    return false;
+  });
+});
