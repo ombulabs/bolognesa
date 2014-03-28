52b9a91784b866f8856ff35f08458ce827d30a28
Lucas Mourelle <info@lucasmourelle.com.ar>
2014-03-28 10:18:07 -0300

    Javascript comments

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -3,16 +3,22 @@
 
 var interval_id;
 
+// Sets the percentage
 function setPercentage(percent){
 	var _percent = Number(percent);
 	$('#timer-progress').attr("value", percent);
 	$('.visible-progress-value').html(Math.floor(percent*100)/100);
 }
 
+// Gets the percentage
+// Returns number
 function getPercentage(){
 	var percent_value = Number( $('#timer-progress').attr("value") );
 	return percent_value;
 }
+
+// It checks if percentage reached 100, if not
+// it adds percentage
 function addPercentage(){
 	if(getPercentage() >= 100){
 		stop();
@@ -24,23 +30,28 @@ function addPercentage(){
 		setPercentage(getPercentage() + percent_to_add);
 	}
 }
+
+// Starts counting working time
 function start(){
 	stop();
 	setPercentage(0);
 	interval_id = setInterval(addPercentage,1000);
   $.ajax("/pomodoris/create");
 }
+
+// Stops timer
 function stop(){
 	clearInterval(interval_id);
 }
+
+// Plays ding sound, using HTML5 <audio> tag
 function playDing(){
 	document.getElementById('audio-ding').currentTime = 0;
 	document.getElementById('audio-ding').load();
 	document.getElementById('audio-ding').play();
 }
 
-
-
+// On ready listeners, events and actions
 $(document).ready(function(){
 	$('.start-button').click(start);
 });
