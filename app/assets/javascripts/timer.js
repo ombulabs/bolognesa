dfc5e33046c857527e3fa856cf5da163c18f75d7
Lucas Mourelle <info@lucasmourelle.com.ar>
2014-04-01 16:56:06 -0300

    Timer is now fixed. Real time when browser window is not in focus.

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -8,14 +8,13 @@ var current_start_date;
 var current_finish_date;
 var current_remaining_time = new Date();
 var current_is_break = false;
-var testing_fast_mode = false;
 var current_total_time_lapse = TIME_LAPSE_WORK;
 
 // CONSTANTS
 var TIME_LAPSE_WORK = 25*60*1000;
 var TIME_LAPSE_BREAK = 5*60*1000;
 var TIME_LAPSE_BREAK_LONG = 15*60*1000;
-
+var TESTING_FAST_MODE = false;
 
 
 // TIMER METHODS
@@ -23,50 +22,84 @@ var TIME_LAPSE_BREAK_LONG = 15*60*1000;
 // Gets the percentage
 // Returns number
 function getPercentage(){
-	var percent_value = 100 - ( current_remaining_time/current_total_time_lapse*100 );
-	return percent_value;
+  var percent_value = 100 - ( current_remaining_time/current_total_time_lapse*100 );
+  return percent_value;
 }
 
 // It checks if percentage reached 100, if not
 // it adds percentage
 function incrementProgress(){
-	if(getPercentage() >= 100){
-		current_remaining_time.setTime(0);
-		view.setPercentage(100, current_remaining_time);
-		if(current_is_break) {
-			finishBreak();
-		} else {
-			finishPomodoro();
-		}
-	} else {
-		if(testing_fast_mode){
-			current_remaining_time.setTime(current_remaining_time.getTime() - 1000*100);
-		} else {
-			current_remaining_time.setTime(current_remaining_time.getTime() - 1000);
-		}
-		view.setPercentage(getPercentage(), current_remaining_time);
-	}
+
+	// Check with real time every time
+	var elapsed = getNow() - current_start_date.getTime();
+  if(TESTING_FAST_MODE){
+		current_remaining_time.setTime(current_total_time_lapse - elapsed*100);
+  } else {
+		current_remaining_time.setTime(current_total_time_lapse - elapsed);
+  }
+
+  if(getPercentage() >= 100){
+    current_remaining_time.setTime(0);
+    view.setPercentage(100, current_remaining_time);
+    if(current_is_break) {
+      finishBreak();
+    } else {
+      finishPomodoro();
+    }
+  } else {
+    view.setPercentage(getPercentage(), current_remaining_time);
+  }
+
+}
+
+// Starts a pomodoro
+function startPomodoro(){
+  current_is_break = false;
+  view.showWorkingButton();
+  $('body').removeClass("break");
+  current_total_time_lapse = TIME_LAPSE_WORK;
+  start();
 }
 
 // Finishes and saves pomodoro
 function finishPomodoro(){
-	stop();
-	$.ajax({
-		type: 'POST',
-		url: "/pomodoris/set_finished"
-	});
-	view.playDing();
-	/* $.ajax({
-		type: 'POST',
-		url: "/pomodoris/set_tags"
-	}); */
-	current_is_break = true;
-	view.showStartButton();
+  stop();
+  $.ajax({
+    type: 'POST',
+    url: "/pomodoris/set_finished"
+  });
+  view.playDing();
+  /* $.ajax({
+    type: 'POST',
+    url: "/pomodoris/set_tags"
+  }); */
+  current_is_break = true;
+  view.showStartButton();
 }
 
-// Starts counting working time
+// Starts a break
+function startBreak(){
+  current_is_break = true;
+  view.showRelaxingButton();
+  $('body').addClass("break");
+  current_total_time_lapse = TIME_LAPSE_BREAK;
+  start();
+}
+
+// Finishes a break
+function finishBreak(){
+  stop();
+  view.playDing();
+  current_total_time_lapse = TIME_LAPSE_WORK;
+  $('body').removeClass("break");
+  view.showStartButton();
+  current_is_break = false;
+}
+
+
+// Starts the timer (work or break)
 function start(){
-	stop();
+  stop();
 
   current_start_date = new Date();
   current_finish_date = new Date();
@@ -74,48 +107,32 @@ function start(){
   current_remaining_time = new Date();
   current_remaining_time.setTime(current_total_time_lapse);
 
-	view.setPercentage(0, current_remaining_time);
-	current_interval_id = setInterval(incrementProgress, 1000);
-	if (!current_is_break) {
-		$.ajax({
-			type: 'POST',
-			url: "/pomodoris/create"
-		});
-	}
+  view.setPercentage(0, current_remaining_time);
+  current_interval_id = setInterval(incrementProgress, 1000);
+  if (!current_is_break) {
+    $.ajax({
+      type: 'POST',
+      url: "/pomodoris/create"
+    });
+  }
 }
 
 // Stops timer
 function stop(){
-	clearInterval(current_interval_id);
-	view.setPercentage(0, current_remaining_time);
+  clearInterval(current_interval_id);
+  view.setPercentage(0, current_remaining_time);
 }
 
-function startPomodoro(){
-	current_is_break = false;
-	view.showWorkingButton();
-	$('body').removeClass("break");
-	current_total_time_lapse = TIME_LAPSE_WORK;
-	start();
-}
-function startBreak(){
-	current_is_break = true;
-	view.showRelaxingButton();
-	$('body').addClass("break");
-	current_total_time_lapse = TIME_LAPSE_BREAK;
-	start();
-}
-function finishBreak(){
-	stop();
-	view.playDing();
-	current_total_time_lapse = TIME_LAPSE_WORK;
-	$('body').removeClass("break");
-	view.showStartButton();
-	current_is_break = false;
-}
+// Starts work or break accordingly
 function startToggle(){
-	if(current_is_break) {
-		startBreak();
-	} else {
-		startPomodoro();
-	}
+  if(current_is_break) {
+    startBreak();
+  } else {
+    startPomodoro();
+  }
+}
+
+// Gets now time
+function getNow() {
+  return ((new Date()).getTime());
 }
