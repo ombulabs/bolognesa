10f5369f81dac16089a28558b2ff300f320441cb
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-04-04 17:04:29 -0300

    Preparing assets pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,8 @@
+//= require jquery-1.11.0.min
+//= require jquery-ui-1.10.4.custom.min
+//= require jquery_ujs
+//= require notices
+//= require timer
+//= require view
+//= require modal
 //= require_tree .
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,8 @@ Bolognesa::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
+  config.assets.enabled = true
+
 end
