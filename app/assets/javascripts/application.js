5254518944029c060a7bc147b85509178c7c9562
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-05-09 12:05:04 -0300

    Javascript alert raised if user leaves when pomodoro is running. Closes #15

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,11 +7,19 @@
 //= require modal
 //= require_tree .
 
+var skip_pomodoro = false;
 // Stop/skip current pomodoro/break using escape key.
 $(document).keyup(function(e) {
     if (e.keyCode == 27) { //escape key
       if(confirm("Skip current pomodoro/break?")) {
         window.location.reload();
+        skip_pomodoro = true;
       }
     }
+});
+
+$(window).bind('beforeunload', function(){
+  if(getPercentage() >= 0 && getPercentage() < 100 && skip_pomodoro == false) {
+    return 'A pomodoro is currently running, and it will be lost if you exit.';
+  }
 });
