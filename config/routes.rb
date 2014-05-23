84e5524a473cdec3a70c76d217b01d40fb4fe6e7
Mauro Otonelli <mauro.otonelli@gmail.com>
2014-05-23 11:12:55 -0300

    Deleted redundant routes.

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,6 @@ Bolognesa::Application.routes.draw do
   match '/auth/failure', :to => 'sessions#failure'
   match "/signout" => "sessions#destroy", :as => :signout
 
-  match "/pomodoris" => "pomodoris#index"
-  match "/pomodoris/create" => "pomodoris#create"
   match "/pomodoris/set_finished" => "pomodoris#set_finished"
   match "/pomodoris/set_tags" => "pomodoris#set_tags"
   match "/today" => "pomodoris#today"
